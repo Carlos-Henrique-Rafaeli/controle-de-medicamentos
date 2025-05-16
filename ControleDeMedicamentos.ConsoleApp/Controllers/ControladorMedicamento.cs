79aeadfbe87a6542cb36c40980067c04377f7cbd
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-05-16 14:32:29 -0300

    Implementado MVC da classe Medicamento

---
+++ ControleDeMedicamentos.ConsoleApp/Controllers/ControladorMedicamento.cs
@@ -0,0 +1,122 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.Extensions;
+using ControleDeMedicamentos.ConsoleApp.Models;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
+using ControleDeMedicamentos.ConsoleApp.ModuloMedicamento;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace ControleDeMedicamentos.ConsoleApp.Controllers;
+
+[Route("medicamentos")]
+public class ControladorMedicamento : Controller
+{
+    private ContextoDados contextoDados;
+    private IRepositorioMedicamento repositorioMedicamento;
+    private IRepositorioFornecedor repositorioFornecedor;
+
+    public ControladorMedicamento()
+    {
+        contextoDados = new ContextoDados(true);
+        repositorioMedicamento = new RepositorioMedicamento(contextoDados);
+        repositorioFornecedor = new RepositorioFornecedor(contextoDados);
+    }
+
+    [HttpGet("cadastrar")]
+    public IActionResult Cadastrar()
+    {
+        var fornecedores = repositorioFornecedor.SelecionarRegistros();
+
+        var cadastrarVM = new CadastrarMedicamentoViewModel(fornecedores);
+
+        return View(cadastrarVM);
+    }
+
+    [HttpPost("cadastrar")]
+    public IActionResult Cadastrar(CadastrarMedicamentoViewModel cadastarVM)
+    {
+        var fornecedores = repositorioFornecedor.SelecionarRegistros();
+
+        Medicamento medicamento = cadastarVM.ParaEntidade(fornecedores);
+
+        repositorioMedicamento.CadastrarRegistro(medicamento);
+
+        var notificacaoVM = new NotificacaoViewModel(
+            "Medicamento Cadastrado!",
+            $"O registro \"{medicamento.Nome}\" foi cadastrado com sucesso!"
+        );
+
+        return View("Notificacao", notificacaoVM);
+    }
+
+    [HttpGet("editar/{id:int}")]
+    public IActionResult Editar([FromRoute] int id)
+    {
+        var medicamento = repositorioMedicamento.SelecionarRegistroPorId(id);
+
+        var fornecedores = repositorioFornecedor.SelecionarRegistros();
+
+        var editarVM = new EditarMedicamentoViewModel(
+            id,
+            medicamento.Nome,
+            medicamento.Descricao,
+            medicamento.Estoque,
+            medicamento.Fornecedor.Id,
+            fornecedores
+        );
+
+        return View(editarVM);
+    }
+
+    [HttpPost("editar/{id:int}")]
+    public IActionResult Editar([FromRoute] int id, EditarMedicamentoViewModel editarVM)
+    {
+        var fornecedores = repositorioFornecedor.SelecionarRegistros();
+
+        var medicamentoOriginal = repositorioMedicamento.SelecionarRegistroPorId(id);
+
+        var medicamentoEditado = editarVM.ParaEntidade(fornecedores);
+
+        repositorioMedicamento.EditarRegistro(id, medicamentoEditado);
+
+        var notificacaoVM = new NotificacaoViewModel(
+            "Medicamento Editado!",
+            $"O registro \"{medicamentoEditado.Nome}\" foi editado com sucesso!"
+        );
+
+        return View("Notificacao", notificacaoVM);
+    }
+
+    [HttpGet("excluir/{id:int}")]
+    public IActionResult Excluir([FromRoute] int id)
+    {
+        var medicamento = repositorioMedicamento.SelecionarRegistroPorId(id);
+
+        var excluirVM = new ExcluirMedicamentoViewModel(medicamento.Id, medicamento.Nome);
+
+        return View(excluirVM);
+    }
+
+    [HttpPost("excluir/{id:int}")]
+    public IActionResult ExcluirConfirmado([FromRoute] int id)
+    {
+        repositorioMedicamento.ExcluirRegistro(id);
+
+        var notificacaoVM = new NotificacaoViewModel(
+            "Medicamento Excluído!",
+            "O registro foi excluído com sucesso!"
+        );
+
+        return View("Notificacao", notificacaoVM);
+    }
+
+    [HttpGet("visualizar")]
+    public IActionResult Visualizar()
+    {
+        var medicamentos = repositorioMedicamento.SelecionarRegistros();
+
+        var visualizarVM = new VisualizarMedicamentosViewModel(medicamentos);
+
+        return View(visualizarVM);
+    }
+}
---
+++ ControleDeMedicamentos.ConsoleApp/Models/MedicamentoViewModels.cs
@@ -0,0 +1,135 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Extensions;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
+using ControleDeMedicamentos.ConsoleApp.ModuloMedicamento;
+using System.Security.Principal;
+
+namespace ControleDeMedicamentos.ConsoleApp.Models;
+
+public abstract class FormularioMedicamentoViewModel
+{
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public int Estoque { get; set; }
+    public int FornecedorId { get; set; }
+
+    public List<SelecionarFornecedorViewModel> FornecedoresDisponiveis { get; set; }
+
+    protected FormularioMedicamentoViewModel()
+    {
+        FornecedoresDisponiveis = new List<SelecionarFornecedorViewModel>();
+    }
+}
+
+public class SelecionarFornecedorViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    public SelecionarFornecedorViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
+public class CadastrarMedicamentoViewModel : FormularioMedicamentoViewModel
+{
+    public CadastrarMedicamentoViewModel() { }
+
+    public CadastrarMedicamentoViewModel(List<Fornecedor> fornecedores) : this()
+    {
+        foreach (var fornecedor in fornecedores)
+        {
+            var selecionarVM = new SelecionarFornecedorViewModel(fornecedor.Id, fornecedor.Nome);
+
+            FornecedoresDisponiveis.Add(selecionarVM);
+        }
+    }
+}
+
+public class EditarMedicamentoViewModel : FormularioMedicamentoViewModel
+{
+    public int Id { get; set; }
+
+    public EditarMedicamentoViewModel() { }
+
+    public EditarMedicamentoViewModel(
+        int id, 
+        string nome, 
+        string descricao, 
+        int estoque, 
+        int fornecedorId, 
+        List<Fornecedor> fornecedores
+        ) : this()
+    {
+        Id = id;
+        Nome = nome;
+        Descricao = descricao;
+        Estoque = estoque;
+        FornecedorId = fornecedorId;
+
+        foreach (var fornecedor in fornecedores)
+        {
+            var selecionarVM = new SelecionarFornecedorViewModel(fornecedor.Id, fornecedor.Nome);
+
+            FornecedoresDisponiveis.Add(selecionarVM);
+        }
+    }
+}
+
+public class ExcluirMedicamentoViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    public ExcluirMedicamentoViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
+public class VisualizarMedicamentosViewModel
+{
+    public List<DetalhesMedicamentoViewModel> Registros { get; set; }
+
+    public VisualizarMedicamentosViewModel(List<Medicamento> medicamentos)
+    {
+        Registros = new List<DetalhesMedicamentoViewModel>();
+
+        foreach (var medicamento in medicamentos)
+        {
+            DetalhesMedicamentoViewModel detalhesVM = medicamento.ParaDetalhesVM();
+
+            Registros.Add(detalhesVM);
+        }
+    }
+}
+
+public class DetalhesMedicamentoViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public int Estoque { get; set; }
+    public string NomeFornecedor { get; set; }
+
+    public DetalhesMedicamentoViewModel(
+        int id, 
+        string nome, 
+        string descricao, 
+        int estoque, 
+        string nomeFornecedor)
+    {
+        Id = id;
+        Nome = nome;
+        Descricao = descricao;
+        Estoque = estoque;
+        NomeFornecedor = nomeFornecedor;
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {Id} | Nome: {Nome} | Descrição: {Descricao} | Estoque: {Estoque} | Fornecedor: {NomeFornecedor}";
+    }
+}
